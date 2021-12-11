825e9c86bb9d3a7b6289823dd18a27f7cb86efe1
finkegabriel <finkegabriel@gmail.com>
2021-12-10 18:41:38 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route,HashRouter } from "react-router-dom";
+import { BrowserRouter, BrowserRouter as Router, Route,Switch } from "react-router-dom";
 
 import Nav from './components/header';
 import Blog from './pages/blog';
@@ -11,11 +11,11 @@ class App extends Component {
     return (
       <Router>
         <Nav />
-        <HashRouter>
+        <Switch>
           <Route exact path="/" component={Home} />
           <Route path="/blog" component={Blog} />
           <Route path="/bat/:id" component={GenerateBlog.GenerateBlog} />
-        </HashRouter>
+        </Switch>
       </Router>
     );
   }
