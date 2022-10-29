350a216b8dc0f05d01494844a24e6ba3e92f7646
finkegabriel <finkegabriel@gmail.com>
2022-10-28 22:44:59 -0700

    update

---
+++ src/posts/HACKERS/photos.js
@@ -0,0 +1,57 @@
+import FirstPhoto from './photos/first.png';
+import CoverPhoto from './photos/cover_photo.png';
+import BodyPhoto from './photos/PXL_20210526_035634485.jpg';
+import BodyPhoto2 from './photos/PXL_20210805_211002056.jpg';
+import BodyPhoto3 from './photos/PXL_20210805_211009113.jpg';
+import BodyPhoto4 from './photos/PXL_20210805_211024765.jpg';
+import BodyPhoto5 from './photos/PXL_20210806_022555057.jpg';
+import BodyPhoto6 from './photos/PXL_20210806_022609036.jpg';
+import BodyPhoto7 from './photos/PXL_20210806_022610431.jpg';
+import BodyPhoto8 from './photos/PXL_20210806_022611704.jpg';
+import BodyPhoto9 from './photos/PXL_20211128_215552921.jpg';
+import BodyPhoto10 from './photos/PXL_20211128_215554300.jpg';
+
+const photos = [
+    {
+        src: FirstPhoto,
+    },
+    {
+        src: CoverPhoto,
+    },
+    {
+        src: BodyPhoto,
+    },
+    {
+        src: BodyPhoto2,
+    }, 
+    {
+        src: BodyPhoto3,
+    },
+    {
+        src: BodyPhoto4,
+    },
+    {
+        src: BodyPhoto5,
+    },
+    {
+        src: BodyPhoto6,
+    },
+    {
+        src: BodyPhoto7,
+    },
+    {
+        src: BodyPhoto8,
+    },
+    {
+        src: BodyPhoto9,
+    },
+    {
+        src: BodyPhoto10,
+    }
+]
+
+export default {
+    FirstPhoto,
+    CoverPhoto,
+    photos,
+}
