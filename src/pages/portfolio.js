96383787352feb316506c70d3c52d00dcce49e6d
finkegabriel <finkegabriel@gmail.com>
2022-11-23 14:09:28 -0700

    update portfolio

---
+++ src/pages/portfolio.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+const baseStyles = {
+    card: {
+        marginTop: '1.5rem',
+        justifyContent: 'space-evenly',
+        alignItems: 'center'
+    },
+    image: {
+        width: '12rem',
+        height: '12rem',
+        borderRadius: '66px',
+    },
+}
+
+class Portfolio extends Component {
+    render() {
+
+        return (
+            <div style={baseStyles.card}>
+                
+            </div>
+
+        )
+    }
+}
+
+export default Portfolio;
