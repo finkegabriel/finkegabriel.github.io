35537bebf9c91b0af99c8904ab3b0b9970c8f3ef
finkegabriel <finkegabriel@gmail.com>
2022-12-09 09:54:42 -0700

    update

--- src/pages/portfolio.js
+++ src/pages/portfolio.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Brief from '../img/brief.jpeg';
 
 const baseStyles = {
     card: {
@@ -11,6 +12,9 @@ const baseStyles = {
         height: '12rem',
         borderRadius: '66px',
     },
+    hr:{
+        width:'50%',
+    }
 }
 
 class Portfolio extends Component {
@@ -18,7 +22,25 @@ class Portfolio extends Component {
 
         return (
             <div style={baseStyles.card}>
-                
+                <br></br>
+                    <center>
+                        <img alt="work" src={Brief} height="50" width="50"></img>
+                    </center>
+                <hr style={baseStyles.hr}></hr>
+                <center>
+                    <a href="/iceddev">
+                        <input type="button" value='Iceddev'></input>
+                    </a>
+                    <a href='/medproject'>
+                        <input type="button" value='MedProject'></input>
+                    </a>
+                    <a href='/bat/0e3e8a27-6e7d-4c6e-bd32-4a1141645f05'>
+                        <input type="button" value='USDA'></input>                    
+                    </a>
+                    <a href=''>
+                        <input type="button" value='ActiveG'></input>
+                    </a>
+                </center>
             </div>
 
         )
