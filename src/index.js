9f5262ed9e125b2b777f386419d4a539f379feca
finkegabriel <finkegabriel@gmail.com>
2023-02-21 18:13:22 -0700

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { BrowserRouter } from "react-router-dom";
 import ReactGA from 'react-ga4';
 const trackingId = "G-B99NR7TNKQ"; // Replace with your Google Analytics tracking ID
-ReactGA.initialize(trackingId);
+ReactGA.initialize([{tracikingId:trackingId}]);
 console.log("tracker ",trackingId);
 ReactDOM.render(
     <BrowserRouter>
