f0ea82ee326bf1a6a5337a3e42c104ffe0e37524
finkegabriel <finkegabriel@gmail.com>
2024-11-27 00:04:51 -0700

    holiday blog

--- src/pages/blog/CHRISTMAS2024.js
+++ src/pages/blog/CHRISTMAS2024.js
@@ -4,8 +4,8 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Grid from '@material-ui/core/Grid';
 import Container from '@material-ui/core/Container';
 import Main from '../../posts/main';
-import Pics from '../../posts/BAG/photos';
-import Gallery from 'react-photo-gallery';
+import Pics from '../../posts/Christmas2024/index';
+// import Gallery from 'react-photo-gallery';
 
 const useStyles = makeStyles((theme) => ({
     mainGrid: {
@@ -15,31 +15,17 @@ const useStyles = makeStyles((theme) => ({
 
 const posts = [];
 
-const photos = [{
-    src: Pics.photos[0].src,
-    width: 1,
-    height: 1,
-}, {
-    src: Pics.photos[1].src,
-    width: 1,
-    height: 1,
-}, {
-    src: Pics.photos[2].src,
-    width: 1,
-    height: 1,
-}, {
-    src: Pics.photos[3].src,
-    width: 1,
-    height: 1,
-}, {
-    src: Pics.photos[4].src,
-    width: 1,
-    height: 1,
-}, {
-    src: Pics.photos[5].src,
-    width: 1,
-    height: 1,
-}];
+const electrical_photos = {
+    src: Pics.photos[0].raspberryPi5,
+    width: 240,
+    height: 150,
+};
+
+const bag_photos = {
+        src:Pics.photos[0].Fiveeleven,
+        width: 240,
+        height: 150,
+}
 
 export default function Bag(props) {
     if (props.loading) {
@@ -57,8 +43,14 @@ export default function Bag(props) {
                             <Main title="Tech Christmas Gift Ideas 2024" posts={posts} />
                             <div style={{ width: '90%', marginLeft: '6%', fontSize: '1rem' }}>
                                 <b>Intro:</b>
-                                <p>If someone in your life is a maker/hacker/tech enthusiast I have curated a list of general items for possible gift ideas.</p>
-                                {/* <Gallery photos={photos} /> */}
+                                <p>If someone in your life is a maker/hacker/tech enthusiast I have curated a list of general items for possible gift ideas. All the items listed is stuff I either own or have had experience with.</p>
+                                <b>Electrical</b>
+                                <h4>Raspberry PI 5</h4>
+                                <a href='https://amzn.to/3OpOAUS'><img src={electrical_photos.src} alt="pi5" width={electrical_photos.width} height={electrical_photos.height}></img></a>
+                                <p><a href='https://amzn.to/3OpOAUS'>Raspberry Pi 5</a></p>
+                                <b>Backpack</b>
+                                <h4>5.11 Rush 24</h4>
+                                <a href='https://amzn.to/3VcUNat'><img src={bag_photos.src} alt="5.11" width={bag_photos.width} height={bag_photos.height}></img></a>
                             </div>
                         </Grid>
                     </main>
--- src/posts/Christmas2024/index.js
+++ src/posts/Christmas2024/index.js
@@ -1,10 +1,36 @@
-
-// import PCBTree from './photos/pcb_tree.png';
 import CoverPhoto from './list.jpg';
+import RaspberryPi5 from './photos/raspberrypi5.jpg';
+import Fiveeleven from './photos/5.11.jpg';
+import Macbook from './photos/2024macbook.jpg';
+import Garmin2 from './photos/garmin2.jpg';
+import Glinet from './photos/glinet.jpg';
+import Klein from './photos/klein.jpg';
+import LegoNasa from './photos/legonasarover.jpg';
+import MasterMind from './photos/mastermind.jpg';
+import Pods2Pro from './photos/podspros.jpg';
+import Pods2gen from './photos/pods2gen.jpg';
+import Pouch from './photos/pouch.jpg';
+import WaterPouch from './photos/waterpouch.jpg'
+import x1nano from './photos/x1nano.jpg';
+import xcool from './photos/xcool.jpg';
 
 const photos = [
     {
         src:CoverPhoto,
+        raspberryPi5:RaspberryPi5,
+        xcool:xcool,
+        x1nano:x1nano,
+        waterPouch:WaterPouch,
+        pouch:Pouch,
+        Pods2Gen:Pods2gen,
+        PodsPro2:Pods2Pro,
+        mastermind:MasterMind,
+        legonasa:LegoNasa,
+        klein:Klein,
+        glinet:Glinet,
+        Garmin2:Garmin2,
+        Macbook:Macbook,
+        Fiveeleven:Fiveeleven
     },
 ]
 
